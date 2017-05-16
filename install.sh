41fe8f8de48d57ddb88006d85d73ca6c32d5de65
Jared Knutson <jared.knutson@protonmail.com>
2017-05-15 19:30:15 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,10 +9,10 @@ echo .tmux_snap
 mv .tmux_snap ~/.tmux_snap
 echo
 mkdir ~/.vim
-cd ~/.vim
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 echo Vim Airline
+cd ~/.vim/bundle/
 git clone https://github.com/vim-airline/vim-airline.git
 git clone https://github.com/vim-airline/vim-airline-themes.git
 echo Vim Color-schemes
